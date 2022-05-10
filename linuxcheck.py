07adaa362a4b965dac7156c5944015aa0dd83b7c
Park jihwan <redplug@gmail.com>
2022-05-10 16:53:00 +0900

    update

--- linuxcheck.py
+++ linuxcheck.py
@@ -1,6 +1,14 @@
-# !/usr/bin/phtyon 
-# -*- coding: UTF-8 -*- 
+# !/usr/bin/phtyon
+# -*- coding: UTF-8 -*-
 
-import os 
+import os
 
-print os.system("ls -al")
+commandlist = [
+    'service walinuxagent status | grep Active',
+    'service walinuxagent status | grep Loaded',
+    'cat /etc/ssh/sshd_config | grep PermitRoot',
+    ''
+]
+for cmd in commandlist:
+    result = os.system(i)
+    print(result)
