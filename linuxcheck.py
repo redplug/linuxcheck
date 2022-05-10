866b4692909baf40a1a36711c90b1098e99d11bb
Park jihwan <redplug@gmail.com>
2022-05-10 17:14:18 +0900

    update

--- linuxcheck.py
+++ linuxcheck.py
@@ -4,16 +4,22 @@
 import os
 
 commandlist = [
+    'hostname',
     'service walinuxagent status | grep Active',
     'service walinuxagent status | grep Loaded',
+    'service waagent status | grep Active',
+    'service waagent status | grep Loaded',
+    'service vasd status | grep Active',
+    'service vasd status | grep Loaded',
     'cat /etc/ssh/sshd_config | grep PermitRoot',
     'ifconfig',
     'cat /etc/*release*',
-    '',
-    '',
-    ''
+    'cat /etc/network/interfaces',
+    'cat /etcn/netplan/*',
+    'cat /etc/systemd/resolved.conf',
+    'cat /etc/resolvconf/resolv.conf.d/tail'
 ]
 for cmd in commandlist:
-    print("\n\n!! START", cmd)
+    print("\n\n!! START\n", cmd)
     os.system(cmd)
-    print("!! END", cmd)
+    print("\n!! END", cmd)
