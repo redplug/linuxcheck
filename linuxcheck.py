3c3eaeed490c417543948ff5fee52000142f1992
Park jihwan <redplug@gmail.com>
2022-05-11 12:26:50 +0900

    update

--- linuxcheck.py
+++ linuxcheck.py
@@ -4,48 +4,56 @@
 import os
 import time
 
-commandlist = [
-    'hostname',
-    'python --version',
-    'python3 --version',
-    'service walinuxagent status | grep Active',
-    'service walinuxagent status | grep Loaded',
-    'service waagent status | grep Active',
-    'service waagent status | grep Loaded',
-    'service vasd status | grep Active',
-    'service vasd status | grep Loaded',
-    'cat /etc/ssh/sshd_config | grep PermitRoot',
-    'ifconfig',
-    'cat /etc/*release*',
-    'cat /etc/network/interfaces',
-    'cat /etc/netplan/*',
-    'cat /etc/systemd/resolved.conf',
-    'cat /etc/resolvconf/resolv.conf.d/tail'
-]
 
-print("1) running everything at once")
-print("2) running step by step (Enter next step)")
-print("3) running 3 second")
-number = int(input("Select Number) "))
-if number == 1:
-    for cmd in commandlist:
-        print("\n!! START !! ", cmd)
-        os.system(cmd)
-        print("\n### END ### ", cmd)
-        print("\n\n")
-elif number == 2:
-    for cmd in commandlist:
-        print("\n!! START !! ", cmd)
-        input("")
-        os.system(cmd)
-        print("\n### END ### ", cmd)
-        print("\n\n")
-elif number == 3:
-    for cmd in commandlist:
-        time.sleep(3)
-        print("\n!! START !! ", cmd)
-        os.system(cmd)
-        print("\n### END ### ", cmd)
-        print("\n\n")
-else:
-    exit()
+def cmdlist():
+    commandlist = [
+        'hostname',
+        'python --version',
+        'python3 --version',
+        'service walinuxagent status | grep Active',
+        'service walinuxagent status | grep Loaded',
+        'service waagent status | grep Active',
+        'service waagent status | grep Loaded',
+        'service vasd status | grep Active',
+        'service vasd status | grep Loaded',
+        'cat /etc/ssh/sshd_config | grep PermitRoot',
+        'ifconfig',
+        'cat /etc/*release*',
+        'cat /etc/network/interfaces',
+        'cat /etc/netplan/*',
+        'cat /etc/systemd/resolved.conf',
+        'cat /etc/resolvconf/resolv.conf.d/tail'
+    ]
+
+
+def CodeRun():
+    print("1) running everything at once")
+    print("2) running step by step (Enter next step)")
+    print("3) running every 3 second")
+    number = int(input("Select Number) "))
+    if number == 1:
+        for cmd in commandlist:
+            print("\n!! START !! ", cmd)
+            os.system(cmd)
+            print("\n### END ### ", cmd)
+            print("\n\n")
+    elif number == 2:
+        for cmd in commandlist:
+            print("\n!! START !! ", cmd)
+            input("")
+            os.system(cmd)
+            print("\n### END ### ", cmd)
+            print("\n\n")
+    elif number == 3:
+        for cmd in commandlist:
+            time.sleep(3)
+            print("\n!! START !! ", cmd)
+            os.system(cmd)
+            print("\n### END ### ", cmd)
+            print("\n\n")
+    else:
+        exit()
+
+
+cmdlist()
+CodeRun()
